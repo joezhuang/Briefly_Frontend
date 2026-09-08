c62d916df3a8f3ad818415b13241c6b5cee2b855
joezhuang <116039184+joezhuang@users.noreply.github.com>
2026-09-08 13:08:46 +1000

    Use PKCE for native Supabase OAuth

--- src/auth/supabase.ts
+++ src/auth/supabase.ts
@@ -14,6 +14,7 @@ export const supabase =
           autoRefreshToken: true,
           persistSession: true,
           detectSessionInUrl: false,
+          flowType: "pkce",
         },
       })
     : null;
